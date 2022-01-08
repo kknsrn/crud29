a5826d200907ba266a0d076445206800cbdc7d53
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-01-08 16:43:36 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,13 +4,14 @@ function Header()
 {
     return(
         <>
-        <div className="navbar navbar-expand-md navbar-info justify-content-sm-start bg-block mb-5">
+        <div className="navbar navbar-expand-md navbar-info justify-content-sm-start bg-dark mb-5">
             <span className="navbar-brand">CRUD</span>
             <button className="navbar-toggler" data-toggle="collapse" data-target="#navcollapse">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navcollapse">
                 <ul className="navbar-nav ml-auto mr-5">
+                    <li className="nav-item mx-3"><NavLink exact to="/" className="nav-link headertext " activeClassName="headerlink">Home</NavLink></li>
                     <li className="nav-item mx-3"><NavLink exact to="/" className="nav-link headertext " activeClassName="headerlink">Users</NavLink></li>
                     <li className="nav-item mx-3"><NavLink to="/createuser" activeClassName="headerlink  " className="nav-link headertext">Create User</NavLink></li>
                 </ul>
